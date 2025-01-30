154e62810eb2f98531f97ec8d96e499342c9f86c
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-01-30 09:12:11 -0700

    Created Pokedex in Controller

--- app/src/main/java/pokedexproject/controller/Controller.java
+++ app/src/main/java/pokedexproject/controller/Controller.java
@@ -1,11 +1,26 @@
 package pokedexproject.controller;
 
-public class Controller {
-   public Controller(){
+import pokedexproject.model.*;
 
+import java.util.ArrayList;
+
+public class Controller {
+   private ArrayList<Pokemon> pokedex;
+
+   public Controller(){
+      this.pokedex = new ArrayList<Pokemon>();
    }
 
    public void start(){
-      System.out.println("Hello");
+      createPokedex();
+   }
+
+   private void createPokedex(){
+      pokedex.add(new Combee());
+      pokedex.add(new Frogadier());
+      pokedex.add(new Lunala());
+      pokedex.add(new Marill());
+      pokedex.add(new Rowlet());
+      pokedex.add(new Snorlax());
    }
 }
