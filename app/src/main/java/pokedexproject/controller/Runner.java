a70d0916deeb7ff060f4207d06e7329c004f4925
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-01-30 08:36:32 -0700

    Added Forgadier pokemon

--- app/src/main/java/pokedexproject/controller/Runner.java
+++ app/src/main/java/pokedexproject/controller/Runner.java
@@ -1,7 +1,5 @@
 package pokedexproject.controller;
 
-import pokedexproject.controller.Controller;
-
 public class Runner {
    public static void main(String[] args){
       Controller controller = new Controller();
---
+++ app/src/main/java/pokedexproject/model/Frogadier.java
@@ -0,0 +1,8 @@
+package pokedexproject.model;
+
+public class Frogadier extends Pokemon {
+   public Frogadier(){
+      super(657, "Frogadier");
+      this.setCanEvolve(true);
+   }
+}
