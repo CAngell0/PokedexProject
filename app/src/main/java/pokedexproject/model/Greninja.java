7b42f21c699fa91211b812aa95a8613a169f9995
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-02-03 07:53:51 -0700

    Added Greninja

---
+++ app/src/main/java/pokedexproject/model/Greninja.java
@@ -0,0 +1,12 @@
+package pokedexproject.model;
+
+public class Greninja extends Frogadier {
+   public Greninja(){
+      super();
+      setPokedexNumber(658);
+   }
+   public Greninja(String name){
+      super(name);
+      setPokedexNumber(658);
+   }
+}
