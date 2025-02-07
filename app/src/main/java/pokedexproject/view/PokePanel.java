569cb51548bc6bb6ee2cff9c05722fd6e489e83f
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-02-07 09:11:09 -0700

    Added data members; need initialization:

--- app/src/main/java/pokedexproject/view/PokePanel.java
+++ app/src/main/java/pokedexproject/view/PokePanel.java
@@ -9,6 +9,25 @@ import java.awt.GridLayout;
 public class PokePanel extends JPanel {
    Controller app;
 
+   private JPanel fieldPanel;
+   private JScrollPane scrollPane;
+   
+   private JButton updateButton;
+   private JCheckBox evolverCheckBox;
+   private JTextArea typeArea;
+
+   private JTextField nameField;
+   private JTextField healthField;
+   private JTextField numberField;
+
+   private JLabel nameLabel;
+   private JLabel healthLabel;
+   private JLabel numberLabel;
+
+   private JLabel imageLabel;
+   private ImageIcon pokemonImage;
+   private JComboBox<String> pokedexSelector;
+
    public PokePanel(Controller app){
       super();
       this.app = app;
